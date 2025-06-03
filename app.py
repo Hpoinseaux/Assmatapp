551c889035b4e604f4f7e56b8c4aea6dff659c9c
Hadrien Poinseaux <hadrien.poinseaux@gmail.com>
2025-06-03 12:19:20 +0200

    up hourstz

--- app.py
+++ app.py
@@ -173,8 +173,8 @@ dossier_photos = "photos"  # ou le dossier cible
 os.makedirs(dossier_photos, exist_ok=True)
 
 parent_enfants = {
-    "Parent Caly": "Caly",
-    "Parent Nate": "Nate"
+    "parent_caly": "Caly",
+    "parent_nate": "Nate"
 }
 tz = timezone('Europe/Paris') 
 authenticator = stauth.Authenticate(credentials, "babyapp_cookie", "random_key", cookie_expiry_days=30)
